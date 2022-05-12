f03d50bc56f52535c1dc856e8fc5c6ddf9bf4eeb
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-05-12 11:25:55 +0200

    Add base files to repository

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+let pokemonList =[
+ {name: 'Ponyta' , height: '99.06', type: 'Fire'},
+ { name: 'Slowpoke', height: '119.38', type: ['Water','Psychic']}, 
+ {name: 'Magnemite', height: '30.48', type: ['Electric', 'Steel']}, 
