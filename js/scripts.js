691569c38bfb5bd4ec3532d1f3ddc3ea2db7e1b9
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-05-12 11:35:32 +0200

    Add missing commas

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@ let pokemonList =[
  {name: 'Ponyta' , height: '99.06', type: 'Fire'},
  { name: 'Slowpoke', height: '119.38', type: ['Water','Psychic']}, 
  {name: 'Magnemite', height: '30.48', type: ['Electric', 'Steel']}, 
- {name: 'Tentacool', height: '88,9', type: ['Water', 'Poison']}
- {name: 'Seel', height: '109.22', type: 'Water'}];
+ {name: 'Tentacool', height: '88,9', type: ['Water', 'Poison']},
+ {name: 'Seel', height: '109.22', type: 'Water'},];
