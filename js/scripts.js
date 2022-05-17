d049e10f5b0e944ab4e9010784db42737bfa66b2
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-05-17 13:04:05 +0200

    Fix Array and if rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
-let pokemonList =[
- {name: 'Ponyta' , height: '99.06', type: 'Fire'},
- {name: 'Slowpoke', height: '119.38', type: ['Water','Psychic']}, 
- {name: 'Magnemite', height: '30.48', type: ['Electric', 'Steel']}, 
- {name: 'Tentacool', height: '88,9', type: ['Water', 'Poison']},
- {name: 'Seel', height: '109.22', type: 'Water'},]
+let pokemonList = [
+    { name: 'Ponyta', height: 99.06, type: ['Fire'] },
+    { name: 'Slowpoke', height: 119.38, type: ['Water', 'Psychic'] },
+    { name: 'Magnemite', height: 30.48, type: ['Electric', 'Steel'] },
+    { name: 'Tentacool', height: 111.9, type: ['Water', 'Poison'] },
+    { name: 'Seel', height: 109.22, type: ['Water'] }
+];
 
 
 //Writes out the Pokemons by checking the length of the list until reaching the last item
- for (let i = 0; i < pokemonList.length; i++ ) {
-     document.write('<br>' + pokemonList[i].name + ' '  + pokemonList[i].height + ' ' ); 
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<br>' + pokemonList[i].name + ' ' + pokemonList[i].height + ' ');
 
-//Make sure to place [i] before .name/height/type, first choose the variable, then the value inside of it.
-if (pokemonList[i].height >= 110) {
-    document.write(' - Wow that is quite big ');
-}
-else if (pokemonList[i].type == 'Water' && 'Poison'){
-    document.write(' - Beware of Poison! You might need an Antidote');
-}
+    //Make sure to place [i] before .name/height/type, first choose the variable, then the value inside of it.
+    if (pokemonList[i].height >= '110') {
+        document.write(' - Wow that is quite big ');
+    }
+    if (pokemonList[i].type.includes('Poison')) {
+        document.write(' - Beware of Poison! You might need an Antidote');
+    }
+
+
 }
 
