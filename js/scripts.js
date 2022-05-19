45f3d4b2dbf32a8389a8043b493072dda3e6085e
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-05-19 13:34:31 +0200

    Update .getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,42 @@
 
-/* let pokemonList = [
-    { name: 'Ponyta', height: 99.06, type: ['Fire'] },
-    { name: 'Slowpoke', height: 119.38, type: ['Water', 'Psychic'] },
-    { name: 'Magnemite', height: 30.48, type: ['Electric', 'Steel'] },
-    { name: 'Tentacool', height: 111.9, type: ['Water', 'Poison'] },
-    { name: 'Seel', height: 109.22, type: ['Water'] }
-];   */
+
+
+
 
 // Declaring variables inside IIFE, the add function and the getAll allow me to access it from outside the function
-let pokemonRepository = (function(){
+let pokemonRepository = (function () {
     let pokemonList = [{ name: 'Ponyta', height: 99.06, type: ['Fire'] },
     { name: 'Slowpoke', height: 119.38, type: ['Water', 'Psychic'] },
     { name: 'Magnemite', height: 30.48, type: ['Electric', 'Steel'] },
     { name: 'Tentacool', height: 111.9, type: ['Water', 'Poison'] },
     { name: 'Seel', height: 109.22, type: ['Water'] }];
     return {
-        add: function(pokemon){
+        add: function (pokemon) {
             pokemonList.push(pokemon);
+
         },
-        getAll: function() {
+        getAll: function () {
             return pokemonList;
         }
     };
 })();
 
-pokemonRepository.add({ name: 'Pikachu', height:10.25, type: 'Electric'});
+pokemonRepository.add({ name: 'Raichu', height: 30.25, type: 'Electric' });
 console.log(pokemonRepository.getAll());
 
 
-    pokemonRepository.getAll().forEach(function (pokemon)
-    {
-        
-        if (pokemon.height >= 110) {
-            document.write('<p>' + 'Name: ' + pokemon.name + ' Height: ' + pokemon.height + ' '  + ' - Wow that is quite big ' + '</p>');
-        }
-        if (pokemon.type.includes('Poison')) {
-            document.write( '-Beware of Poison! You might need an Antidote' + '<br>' +'<br>');
-        }
-        else if (pokemon.height < 110) {
-            document.write('<p>' + 'Name: ' + pokemon.name + ' Height: ' + pokemon.height + ' ' + '</p>');
+pokemonRepository.getAll().forEach(function (pokemon) {
 
-        }
-    
+    if (pokemon.height >= 110) {
+        document.write('<p>' + 'Name: ' + pokemon.name + ' Height: ' + pokemon.height + ' ' + ' - Wow that is quite big ' + '</p>');
+    }
+    if (pokemon.type.includes('Poison')) {
+        document.write('-Beware of Poison! You might need an Antidote' + '<br>' + '<br>');
+    }
+    else if (pokemon.height < 110) {
+        document.write('<p>' + 'Name: ' + pokemon.name + ' Height: ' + pokemon.height + ' ' + '</p>');
 
-
-
-
+    }
 
 });
-
 
